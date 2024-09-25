f8aa1d304322406f9f97156147099f1bef0f8b4b
Michael Drennen <michaeldrennen74@gmail.com>
2024-09-24 18:08:09 -0600

    Created PHPUnit test class.

---
+++ tests/DataHelperTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MichaelDrennen\DataHelper\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+
+
+class DataHelperTest extends TestCase {
+    public function setUp(): void {}
+
+
+    public function tearDown(): void {}
+
+
+
+}
